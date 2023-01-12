752f753eac64a77b580adbeae11b5091d8838242
Travis Faas <travis.faas@gmail.com>
2023-01-12 15:54:17 -0500

    Creating base project structure

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(800,600);
+    background(227, 179, 66);
+
+    fill (76, 19, 102);
+    rect(40,50,100,20);
+}
+
+function draw() {
+    circle(mouseX, mouseY, 17);
+}
